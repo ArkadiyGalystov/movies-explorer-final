0aa2be7abbbdc7a815f25b0466088d1ac40e4c49
ArkadiyGalystov <119405763+ArkadiyGalystov@users.noreply.github.com>
2023-09-26 19:09:29 +0300

    Update constants.js

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -2,7 +2,7 @@ const CREATED_CODE = 201;
 
 const REGEX_URL_EMAIL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
-const SECRET_KEY_DEV = 'dev-secret';
+const SECRET_KEY_DEV = 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b';
 
 module.exports = {
   CREATED_CODE,
