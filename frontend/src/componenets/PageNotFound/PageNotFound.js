7c18b54487de1670b403771f3d97d4d88b2520fb
ArkadiyGalystov <119405763+ArkadiyGalystov@users.noreply.github.com>
2023-09-26 23:18:59 +0300

    Update PageNotFound.js

--- frontend/src/componenets/PageNotFound/PageNotFound.js
+++ frontend/src/componenets/PageNotFound/PageNotFound.js
@@ -1,20 +1,16 @@
 import React from "react"
 import "./PageNotFound.css"
-import { useNavigate } from "react-router-dom"
-import { Link } from "react-scroll"
+import { Link, useNavigate } from "react-router-dom"
 
-function PageNotFound() {
-  const navigate = useNavigate()
-  function navigateBack() {
-    navigate(-1)
-  }
+const PageNotFound = () => {
+  const navigate = useNavigate();
 
   return (
     <main>
       <section className="notfound-error">
         <h1 className="notfound-error__title">404</h1>
         <p className="notfound-error__subtitle">Страница не найдена</p>
-        <Link onClick={navigateBack} className="notfound-error__link">Назад</Link>
+        <Link className="notfound-error__link" onClick={() => navigate(-1)}>Назад</Link>
       </section>
     </main>
   )
