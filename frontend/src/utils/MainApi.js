093735f23493fc44d327a7e91b9332e3c792d7b6
ArkadiyGalystov <119405763+ArkadiyGalystov@users.noreply.github.com>
2023-09-26 15:04:02 +0300

    Update MainApi.js

--- frontend/src/utils/MainApi.js
+++ frontend/src/utils/MainApi.js
@@ -1,7 +1,7 @@
 import { getCheckResponse } from "./functionHelpers"
 
-//export const BASE_URL = 'https://';
-export const BASE_URL = "http://localhost:3001"
+export const BASE_URL = 'https://ark.nomoredomainsicu.ru';
+//export const BASE_URL = "http://localhost:3001"
 
 export const getContent = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
